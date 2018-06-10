c48f7ddf8f669aa101cdd8a678f776f22d83e00b
HP <HP@Sachin-PC>
2018-06-10 18:08:52 +0530

    2save changes

--- FirstApplication1/src/com/nt/test/Demo.java
+++ FirstApplication1/src/com/nt/test/Demo.java
@@ -2,9 +2,16 @@ package com.nt.test;
 
 public class Demo {
 	
+	public static String wishMsg(){
+		return "Good Evening";
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("hello, how r u?");
 		System.out.println("bye bye...");
+		
+		wishMsg();
+		
 	}
 
 }
