a341bf63cb370415370abe5624134379115819a5
HP <HP@Sachin-PC>
2018-06-10 18:18:22 +0530

    save3changes

--- FirstApplication1/src/com/nt/test/Demo.java
+++ FirstApplication1/src/com/nt/test/Demo.java
@@ -7,10 +7,15 @@ public class Demo {
 	}
 	
 	public static void main(String[] args) {
+		
 		System.out.println("hello, how r u?");
 		System.out.println("bye bye...");
 		
 		wishMsg();
+		
+		CountryandLanguage cl=new CountryandLanguage();
+		cl.m1();
+		cl.m2();
 		
 	}
 
